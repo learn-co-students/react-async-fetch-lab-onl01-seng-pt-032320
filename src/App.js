da93cecaf02b458953f9910cf607755ee623373c
nabilhayet <nabil4457@yahoo.com>
2020-12-12 20:22:41 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { PureComponent } from 'react';
+
+class App extends React.PureComponent {
+constructor(props){
+    super(props)
+    state = {
+        list: []
+    }
+}
+componentDidMount(){
+    fetch("http://api.open-notify.org/astros.json")
+    .then(response => response.json())
+    .then(data => {
+        this.setState({
+            list: data.people 
+        })
+    })
+}
+render() {
+    return (
+        <div>
+            {this.state.list.map(person => person.name)}
+        </div>
+    )
+  }
+}
+export default App 
