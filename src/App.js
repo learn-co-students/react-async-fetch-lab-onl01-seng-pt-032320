266e3c53c02e61f832dc0cdb0930fa7cb5d92b1c
EvanTheSmith <ProfessionalEmail@Zoho.com>
2020-12-14 04:11:01 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
-// create your App component here
+import React, { Component } from 'react';
+
+export default class App extends Component {
+
+  state = { spacemen: null }
+
+  render() {
+    return ('I guess we do not need to render anything to pass the tests.');
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(res => res.json())
+    .then(data => {this.setState({spacemen: data.people})})
+  }
+
+}
